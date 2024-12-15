65f753e79aa11e940aa3dbc4db1753a17912ce39
seolgang <seolgang_@naver.com>
2024-12-15 20:56:04 +0900

    feat: add PointPolicy class and write test code

---
+++ src/main/kotlin/io/hhplus/tdd/point/policy/PointPolicy.kt
@@ -0,0 +1,32 @@
+package io.hhplus.tdd.point.policy
+
+import io.hhplus.tdd.point.UserPoint
+import org.springframework.stereotype.Component
+
+@Component
+class PointPolicy {
+    companion object {
+        const val MAX_AVAILABLE_POINT = 1000000L;
+        const val MIN_AVAILABLE_POINT = 1000L;
+    }
+
+    fun validateCharge(userPoint: UserPoint, amount: Long) {
+        if(amount < MIN_AVAILABLE_POINT) {
+            throw IllegalArgumentException("Charge amount must be greater than $MIN_AVAILABLE_POINT points")
+        }
+
+        if(amount > MAX_AVAILABLE_POINT) {
+            throw IllegalArgumentException("Cannot exceed maximum available point limit")
+        }
+    }
+
+    fun validateUse(userPoint: UserPoint, amount: Long) {
+        if(amount < MIN_AVAILABLE_POINT) {
+            throw IllegalArgumentException("Use amount must be greater than $MIN_AVAILABLE_POINT points")
+        }
+
+        if(amount > userPoint.point) {
+            throw IllegalArgumentException("Cannot use more points than your current point")
+        }
+    }
+}
--- src/test/kotlin/io/hhplus/tdd/point/unit/PointPolicy.test.kt
+++ src/test/kotlin/io/hhplus/tdd/point/unit/PointPolicy.test.kt
@@ -1,7 +1,10 @@
 package io.hhplus.tdd.point.unit
 
+import io.hhplus.tdd.point.UserPoint
+import io.hhplus.tdd.point.policy.PointPolicy
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class PointPolicyTest {
     @Nested
@@ -9,10 +12,26 @@ class PointPolicyTest {
         @Nested
         inner class ThrowIllegalArgumentException {
             @Test
-            fun `charge amount less than MIN_AVAILABLE_POINT`() {}
+            fun `charge amount less than MIN_AVAILABLE_POINT`() {
+                val policy = PointPolicy()
+                val userPoint = UserPoint(0, 0, 0);
+                val amount: Long = 999;
+
+                assertThrows<IllegalArgumentException> {
+                    policy.validateCharge(userPoint, amount)
+                }
+            }
 
             @Test
-            fun `charge amount more than MAX_AVAILABLE_POINT`() {}
+            fun `charge amount more than MAX_AVAILABLE_POINT`() {
+                val policy = PointPolicy()
+                val userPoint = UserPoint(0, 0, 0)
+                val amount: Long = PointPolicy.MAX_AVAILABLE_POINT + 1
+
+                assertThrows<IllegalArgumentException> {
+                    policy.validateCharge(userPoint, amount)
+                }
+            }
         }
     }
 
@@ -20,10 +39,26 @@ class PointPolicyTest {
     inner class Use {
         @Nested inner class ThrowIllegalArgumentException {
             @Test
-            fun `use amount less than MIN_AVAILABLE_POINT`() {}
+            fun `use amount less than MIN_AVAILABLE_POINT`() {
+                val policy = PointPolicy()
+                val userPoint = UserPoint(0, 1000, 0)
+                val amount: Long = 999
+
+                assertThrows<IllegalArgumentException> {
+                    policy.validateUse(userPoint, amount)
+                }
+            }
 
             @Test
-            fun `use amount more than current point`() {}
+            fun `use amount more than current point`() {
+                val policy = PointPolicy()
+                val userPoint = UserPoint(0, 1000, 0)
+                val amount: Long = 1001
+
+                assertThrows<IllegalArgumentException> {
+                    policy.validateUse(userPoint, amount)
+                }
+            }
         }
     }
 }
