68f74099acc32c5b4d0b832993b3191949010673
seolgang <seolgang_@naver.com>
2024-12-15 19:59:16 +0900

    feat: add PointHistoryRepository

---
+++ src/main/kotlin/io/hhplus/tdd/point/repository/PointHistoryRepository.kt
@@ -0,0 +1,17 @@
+package io.hhplus.tdd.point.repository
+
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.point.PointHistory
+import io.hhplus.tdd.point.TransactionType
+import org.springframework.stereotype.Repository
+
+@Repository
+class PointHistoryRepository(private val pointHistoryConn: PointHistoryTable) {
+    fun create(userId: Long, amount: Long, transactionType: TransactionType): PointHistory {
+        return pointHistoryConn.insert(userId, amount, transactionType, 0);
+    }
+
+    fun findAllByUserId(userId: Long): List<PointHistory> {
+        return pointHistoryConn.selectAllByUserId(userId);
+    }
+}
