640bacea73f48ed92e093041b352e516e5ba1a4d
seolgang <seolgang_@naver.com>
2024-12-16 22:11:33 +0900

    feat: define PointService test code

---
+++ src/test/kotlin/io/hhplus/tdd/point/integration/PointService.test.kt
@@ -0,0 +1,21 @@
+package io.hhplus.tdd.point.integration
+
+import org.junit.jupiter.api.Test
+
+class PointServiceTest {
+    @Test
+    fun `should charge point`() {
+    }
+
+    @Test
+    fun `should use point`() {
+    }
+
+    @Test
+    fun `should get user point`() {
+    }
+
+    @Test
+    fun `should get user point history`() {
+    }
+}
